23ad5f04d68f336194372ce6a0dd37856239de86
Vladimir Gorbatenko <mail.gva@gmail.com>
2021-01-17 21:28:44 +0200

    second commit

--- src/main/java/com/gorbatenko/filter/HappyFilter.java
+++ src/main/java/com/gorbatenko/filter/HappyFilter.java
@@ -1,18 +1,23 @@
 package com.gorbatenko.filter;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class HappyFilter extends GenericFilterBean {
+public class HappyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
